34a90b21d50e2b9a2907f3697b29a11b83b7f903
Pöschl <poeschl_markus@web.de>
2014-12-05 00:38:18 +0100

    Init Project

---
+++ app/src/androidTest/java/de/poeschl/apps/debuganddelete/ApplicationTest.java
@@ -0,0 +1,13 @@
+package de.poeschl.apps.debuganddelete;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
