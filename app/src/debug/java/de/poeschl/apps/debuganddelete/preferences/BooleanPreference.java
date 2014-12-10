c59db1593f0cc2e49859b844d08bdf6002f2e2ee
Pöschl <poeschl_markus@web.de>
2014-12-10 20:02:03 +0100

    Applied copyright to files

--- app/src/debug/java/de/poeschl/apps/debuganddelete/preferences/BooleanPreference.java
+++ app/src/debug/java/de/poeschl/apps/debuganddelete/preferences/BooleanPreference.java
@@ -1,29 +1,36 @@
+/*
+ * Copyright 2014 Markus Pöschl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.poeschl.apps.debuganddelete.preferences;
 
 import android.content.SharedPreferences;
 
-/**
- * Taken from the sample app u2020 from Jake Wharton.
- *
- * @see <a href="https://github.com/JakeWharton/u2020">GitHub</a>
- */
 public class BooleanPreference {
     private final SharedPreferences preferences;
     private final String key;
-    private final boolean defaultValue;
+    private final boolean defaultVal;
 
-    public BooleanPreference(SharedPreferences preferences, String key) {
-        this(preferences, key, false);
-    }
-
-    public BooleanPreference(SharedPreferences preferences, String key, boolean defaultValue) {
+    public BooleanPreference(SharedPreferences preferences, String key, boolean defaultVal) {
         this.preferences = preferences;
         this.key = key;
-        this.defaultValue = defaultValue;
+        this.defaultVal = defaultVal;
     }
 
     public boolean get() {
-        return preferences.getBoolean(key, defaultValue);
+        return preferences.getBoolean(key, defaultVal);
     }
 
     public boolean isSet() {
--- app/src/release/java/de/poeschl/apps/debuganddelete/Modules.java
+++ app/src/release/java/de/poeschl/apps/debuganddelete/Modules.java
@@ -4,7 +4,7 @@ import de.poeschl.apps.debuganddelete.AppModule;
 import de.poeschl.apps.debuganddelete.DebugAndDeleteApp;
 
 /**
- * Created by markus on 05.12.14.
+ * Created by Markus Pöschl on 05.12.14.
  */
 public class Modules {
 
