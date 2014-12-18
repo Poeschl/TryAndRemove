5d56e045661aa393bfc640a2be8334eb97ef9668
Poeschl <poeschl_markus@web.de>
2014-12-18 12:21:01 +0100

    Removed unnessesary toasts

--- app/src/main/java/de/poeschl/apps/tryandremove/activities/AppListActivity.java
+++ app/src/main/java/de/poeschl/apps/tryandremove/activities/AppListActivity.java
@@ -118,7 +118,14 @@ public class AppListActivity extends TryAndRemoveActivity {
             unregisterReceiver();
         }
         Timber.d("Listener Status: " + newTrackState);
-        setRecordButtonState(newTrackState);
+        setRecordState(newTrackState);
+    }
+
+    private void setRecordState(boolean state) {
+        if (isTracking.get() == state) {
+            return;
+        }
+        setRecordButtonState(state);
     }
 
     private void setRecordButtonState(boolean active) {
