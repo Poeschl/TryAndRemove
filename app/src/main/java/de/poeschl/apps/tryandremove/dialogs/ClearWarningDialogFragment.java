0656212efad8b9a151f6ae17037886995cf1cf3b
Poeschl <poeschl_markus@web.de>
2015-01-04 13:46:15 +0100

    Changed to orignal fragements because > Api 13

--- app/src/main/java/de/poeschl/apps/tryandremove/dialogs/ClearWarningDialogFragment.java
+++ app/src/main/java/de/poeschl/apps/tryandremove/dialogs/ClearWarningDialogFragment.java
@@ -18,10 +18,10 @@ package de.poeschl.apps.tryandremove.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
 
 import de.poeschl.apps.tryandremove.R;
 
